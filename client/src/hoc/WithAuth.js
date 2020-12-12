2be216673926ab5f69419a180e1a35473ee5a70c
Ivan Alenin <aisproect@yandex.ru>
2020-12-13 00:44:58 +0300

    Get user info (first part, logic only)

--- client/src/hoc/WithAuth.js
+++ client/src/hoc/WithAuth.js
@@ -2,34 +2,10 @@ import React from 'react'
 
 import {useAuth} from '../context/authContext'
 
-const storageName = 'auth'
-
-
 const withAuth = ComposedComponent => {
     return ((props) => {
         const {user, updateUser, login, logout} = useAuth()
 
-        // console.log(user)
-        //
-        // const data = JSON.parse(localStorage.getItem(storageName))
-        //
-        // let newUser
-        // if (data && data.token) {
-        //     newUser = {
-        //         authState: !!data.token,
-        //         client_id: data.client_id,
-        //         token: data.token,
-        //         refresh_token: data.refresh_token,
-        //         login: login,
-        //         logout: logout
-        //     }
-        // } else {
-        //     newUser = {
-        //         authState: false,
-        //         login: login,
-        //         logout: logout
-        //     }
-        // }
           const newUser = {
                 ...user,
                 login: login,
@@ -41,55 +17,3 @@ const withAuth = ComposedComponent => {
 }
 
 export default withAuth
-
-// const withAuth = ComposedComponent => {
-//     return ((props) => {
-//         const {auth, updateAuth} = useAuth()
-//
-//         const login = useCallback((client_id, token, refresh_token) => {
-//             const newUser = {
-//                 authState: !!token,
-//                 client_id: client_id,
-//                 token: token,
-//                 refresh_token: refresh_token
-//             }
-//
-//             localStorage.setItem(storageName,
-//                 JSON.stringify({
-//                     client_id: client_id,
-//                     token: token,
-//                     refresh_token: refresh_token
-//                 }))
-//
-//             updateAuth(newUser)
-//         }, [])
-//
-//         const logout = useCallback(() => {
-//             localStorage.removeItem(storageName)
-//             updateAuth(newUser)
-//         }, [])
-//
-//         const data = JSON.parse(localStorage.getItem(storageName))
-//
-//         let newUser
-//         if (data && data.token) {
-//             newUser = {
-//                 authState: !!data.token,
-//                 client_id: data.client_id,
-//                 token: data.token,
-//                 refresh_token: data.refresh_token,
-//                 login: login,
-//                 logout: logout
-//             }
-//         } else {
-//             newUser = {
-//                 authState: false,
-//                 login: login,
-//                 logout: logout
-//             }
-//         }
-//
-//         return <ComposedComponent user={newUser} {...props}/>
-//     })
-// }
-
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Container} from 'react-bootstrap'
+import axios from "axios";
+import withAuth from "../hoc/WithAuth";
 
 const HomePage = props => {
-  return (
-    <Container className="my-2">
-      <h2>Home page</h2>
-    </Container>
-  )
+    const {user} = props
+    const [info, setInfo] = useState()
+
+    //http://erp.apptrix.ru/api/clients/RU-000000/
+    useEffect(() => {
+        axios.get(`http://erp.apptrix.ru/api/clients/${user.client_id}/`)
+            .then(res => {
+                if (res.status === 200) {
+                    const data = res.data
+                    setInfo(data)
+                }
+                console.log('response', res)
+            })
+            .catch(e => console.log('error', e))
+    }, [])
+
+    return (
+        <Container className="my-2">
+            <h2>Home page</h2>
+            <p>{!!info && JSON.stringify(info)}</p>
+        </Container>
+    )
 }
 
-export default HomePage
+export default withAuth(HomePage)
